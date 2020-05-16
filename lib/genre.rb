1e299582915a9081720bb24a1b512d2e3eb58746
guerino antonini <guerantonini94@gmail.com>
2020-05-16 01:28:13 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,20 +1,26 @@
-class Genre 
-  attr_accessor :name 
+class Genre
+  attr_accessor :name
+
   @@all = []
-  
+
   def initialize(name)
     @name = name
-    @@all << self 
-  end 
-  
-  def self.all 
-    @@all 
-  end 
-  
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
   def songs
     Song.all { |song| song.genre == self }
   end
 
+  def artists
+    songs.map(&:artist)
+  end
 
-
-end 
+  def add_song(song)
+    songs << song
+  end
+end
